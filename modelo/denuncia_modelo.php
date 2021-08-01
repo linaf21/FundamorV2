bf4e9780688f04020502dd67381f885cc267ebc8
ACZuluaga130900 <55557441+ACZuluaga130900@users.noreply.github.com>
2021-08-01 01:04:57 -0500

    --

--- modelo/denuncia_modelo.php
+++ modelo/denuncia_modelo.php
@@ -19,6 +19,8 @@
         
         public function set_reporte()
         {
+            
+            echo '<script language="javascript">alert("HOLA");</script>';
             $id_reporte=null;
             $fecha=null;
             $municipio = $_POST["select_usuario"];

9b1b36a726f87694bcd5ac23178a92c8b2051e7e
ACZuluaga130900 <55557441+ACZuluaga130900@users.noreply.github.com>
2021-08-01 15:14:57 -0500

    --

--- modelo/denuncia_modelo.php
+++ modelo/denuncia_modelo.php
@@ -15,7 +15,9 @@
             $this->municipios=array();
             $this->documentos=array();
         }
-
+        function phpAlert($msg) {
+            echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+        }
         
         public function set_reporte()
         {   
@@ -54,6 +56,7 @@
             $sql->bind_param('isisssssssiisssssssss',$id_reporte,$fecha,$tipo_documento,$adjunto,$nombres,$apellidos,$documento,$email,$telefono,$descripcion,
             $departamento,$municipio,$direccion,$v1,$v2,$v3,$v4,$v5,$v6,$v7,$v8);
             $sql->execute();
+            phpAlert($sql);
         }
         public function get_municipios()
         {
